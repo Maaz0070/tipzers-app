579b16475c4664eed4d1dfd52974b401aecb6b77
Maaz Adil <maazadil786@gmail.com>
2020-08-01 14:49:17 -0700

    Initial Commit

---
+++ tipzers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipzers
+//
+//  Created by Maaz Adil on 8/1/20.
+//  Copyright © 2020 Maaz Adil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
